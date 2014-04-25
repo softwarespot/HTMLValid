aadde92f6ad7170ef9a7660c408987115d194acc
SoftwareSpot Apps <softwarespot@users.noreply.github.com>
2014-04-25 23:53:47 +0300

    Added: HTMLValid application.

---
+++ HTMLValid/HTMLValid.cs
@@ -0,0 +1,225 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Text;
+
+namespace HTMLValid
+{
+    public enum HTMLValidFileType : byte
+    {
+        CSS,
+        HTML,
+        URL,
+        NonSupportedFile
+    }
+
+    public enum HTMLValidStatus : byte
+    {
+        Abort,
+        Invalid,
+        Valid
+    }
+
+    internal struct HTMLValidResults
+    {
+        public readonly int Errors, Warnings;
+
+        public readonly HTMLValidFileType FileType;
+
+        // ReadOnly instance fields.
+        public readonly bool IsConnected;
+
+        public readonly HTMLValidStatus Status;
+
+        public HTMLValidResults(bool isConnected, HTMLValidStatus status, HTMLValidFileType fileType, int errors, int warnings) // Constructor.
+        {
+            IsConnected = isConnected;
+            FileType = fileType;
+            Status = status;
+            Errors = errors;
+            Warnings = warnings;
+        }
+    }
+
+    internal class HTMLValid
+    {
+        private string userAgent = string.Empty;
+
+        public HTMLValid()
+        {
+            UserAgent = null;
+        }
+
+        public HTMLValid(string userAgent)
+        {
+            UserAgent = userAgent;
+        }
+
+        public string UserAgent
+        {
+            get
+            {
+                return userAgent;
+            }
+            set
+            {
+                if (!string.IsNullOrWhiteSpace(value)) // If not null or whitespace then set the UserAgent.
+                {
+                    userAgent = value;
+                }
+                else
+                {
+                    userAgent = "HTMLValid";
+                }
+            }
+        }
+
+        public HTMLValidResults ValidateFilePath(string filePath, bool isReportSource)
+        {
+            // Values that will later be passed to HTMLValidResults().
+            bool isConnected = false;
+            int errors = 0, warnings = 0;
+            HTMLValidFileType fileType = HTMLValidFileType.NonSupportedFile;
+            HTMLValidStatus status = HTMLValidStatus.Abort;
+
+            bool isCSS = IsCSSFile(filePath), isHTML = IsHTMLFile(filePath);
+            if (isCSS || isHTML)
+            {
+                string fileParams = File.ReadAllText(filePath, Encoding.UTF8); // Read the file using UTF8 encoding.
+                fileParams = Uri.EscapeDataString(fileParams); // Encode the file data by escaping certain characters.
+
+                HttpWebResponse webResponse = null;
+                if (isCSS)
+                {
+                    fileType = HTMLValidFileType.CSS;
+                    fileParams = "?text=" + fileParams + (isReportSource ? "&ss=1" : "");
+                    webResponse = HttpGet(@"http://jigsaw.w3.org/css-validator/validator", fileParams);
+                }
+                else
+                {
+                    fileType = HTMLValidFileType.HTML;
+                    fileParams = "uploaded_file=" + fileParams + (isReportSource ? "&output=text" : "");
+                    webResponse = HttpPost(@"http://validator.w3.org/check", fileParams);
+                }
+
+                if (webResponse != null)
+                {
+                    using (webResponse)
+                    {
+                        isConnected = IsOnline(webResponse.StatusCode); // Set the IsOnline property.
+
+                        // Parsing the source is a better approach, but for some reason the regular expressions were causing an exception.
+                        int.TryParse(webResponse.GetResponseHeader("X-W3C-Validator-Errors"), out errors); // Set the number of errors field.
+                        switch (webResponse.GetResponseHeader("X-W3C-Validator-Status").ToLower())
+                        {
+                            case "valid":
+                                status = HTMLValidStatus.Valid;
+                                break;
+
+                            case "invalid":
+                                status = HTMLValidStatus.Invalid;
+                                break;
+
+                            case "abort":
+                                status = HTMLValidStatus.Abort;
+                                break;
+
+                            default:
+                                status = HTMLValidStatus.Abort;
+                                break;
+                        }
+                        if (isHTML) // Only returned when validating a HTML file.
+                        {
+                            int.TryParse(webResponse.GetResponseHeader("X-W3C-Validator-Warnings"), out warnings); // Set the number of warnings field.
+                        }
+                    }
+                }
+            }
+            return new HTMLValidResults(isConnected, status, fileType, errors, warnings);
+        }
+
+        private HttpWebResponse HttpGet(string url, string parameters)
+        {
+            HttpWebResponse webResponse = null;
+            try
+            {
+                HttpWebRequest webRequest = (HttpWebRequest)WebRequest.Create(url + parameters);
+                webRequest.Method = "GET";
+                webRequest.UserAgent = UserAgent;
+                webResponse = (HttpWebResponse)webRequest.GetResponse();
+            }
+            catch { }
+            return webResponse;
+        }
+
+        private HttpWebResponse HttpPost(string url, string parameters)
+        {
+            HttpWebResponse webResponse = null;
+            try
+            {
+                HttpWebRequest webRequest = (HttpWebRequest)WebRequest.Create(url);
+                webRequest.ContentType = "application/x-www-form-urlencoded";
+                webRequest.Method = "POST";
+                webRequest.UserAgent = UserAgent;
+
+                byte[] postData = Encoding.UTF8.GetBytes(parameters); // Create a byte array of data to write to the request stream.
+                webRequest.ContentLength = postData.Length; // Set the Content-Length of the header.
+
+                using (Stream webStream = webRequest.GetRequestStream())
+                {
+                    webStream.Write(postData, 0, postData.Length);
+                }
+                webResponse = (HttpWebResponse)webRequest.GetResponse(); // Get the response from the server.
+            }
+            catch { }
+            return webResponse;
+        }
+
+        private bool IsCSSFile(string filePath)
+        {
+            return File.Exists(filePath) && filePath.EndsWith(".css", StringComparison.OrdinalIgnoreCase) && new FileInfo(filePath).Length > 0;
+        }
+
+        private bool IsHTMLFile(string filePath)
+        {
+            return File.Exists(filePath) && (filePath.EndsWith(".htm", StringComparison.OrdinalIgnoreCase) || filePath.EndsWith(".html", StringComparison.OrdinalIgnoreCase) && new FileInfo(filePath).Length > 0);
+        }
+
+        private bool IsOnline(HttpStatusCode statusCode) // Parse the status code to see if it was a good response.
+        {
+            HttpStatusCode[] httpGoodStatuses = { HttpStatusCode.OK, HttpStatusCode.Moved, HttpStatusCode.Redirect, HttpStatusCode.RedirectMethod, HttpStatusCode.NotModified, HttpStatusCode.RedirectKeepVerb  ,
+                                   HttpStatusCode.Unauthorized, HttpStatusCode.Forbidden, HttpStatusCode.MethodNotAllowed };
+            foreach (HttpStatusCode httpGoodStatus in httpGoodStatuses)
+            {
+                if (statusCode == httpGoodStatus)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        private bool IsURL(string url)
+        {
+            Uri urlResult;
+            return Uri.TryCreate(url, UriKind.Absolute, out urlResult) && urlResult.Scheme == Uri.UriSchemeHttp;
+        }
+
+        private HTMLValidResults ValidateURL(string urlPath, bool isReportSource) // Currently not implemented.
+        {
+            // Values that will later be passed to HTMLValidResults().
+            bool isConnected = false;
+            int errors = 0, warnings = 0;
+            HTMLValidFileType fileType = HTMLValidFileType.NonSupportedFile;
+            HTMLValidStatus status = HTMLValidStatus.Abort;
+
+            if (IsURL(urlPath))
+            {
+                fileType = HTMLValidFileType.URL;
+                string fileData = "uri=" + Uri.EscapeUriString(urlPath); // Escape the URL.
+            }
+
+            return new HTMLValidResults(isConnected, status, fileType, errors, warnings);
+        }
+    }
+}
---
+++ HTMLValid/Program.cs
@@ -0,0 +1,228 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Text.RegularExpressions;
+using System.Threading;
+
+namespace HTMLValid
+{
+    internal class Program
+    {
+        private static void Exit(sbyte errorCode)
+        {
+            Console.WriteLine(); // Empty line.
+            Console.Write("Press any key to continue . . .");
+            Console.ReadKey(true);
+            Environment.Exit(errorCode);
+        }
+
+        private static bool GetCommandLineArgs(string[] commandlineArgs, ref string filePath, ref bool isErrorsWarningOnly)
+        {
+            bool ret = false;
+            foreach (string commandline in commandlineArgs)
+            {
+                if (File.Exists(commandline))
+                {
+                    filePath = commandline;
+                    ret = true; // A search path was found.
+                    continue;
+                }
+
+                if (commandline.ToLower() == "-allfiles")
+                {
+                    isErrorsWarningOnly = !isErrorsWarningOnly; // Opposite of what was passed.
+                    continue;
+                }
+            }
+            return ret;
+        }
+
+        private static bool IsDir(string filePath)
+        {
+            return Directory.Exists(filePath) && !File.Exists(filePath);
+        }
+
+        private static bool IsFile(string filePath)
+        {
+            return File.Exists(filePath) && !Directory.Exists(filePath);
+        }
+
+        private static void Main(string[] commandlineArgs)
+        {
+            // Set the enironment variables for the application.
+            Environment.SetEnvironmentVariable("PROGRAMNAME", "HTMLValid");
+            Environment.SetEnvironmentVariable("FILEVERSION", "0.0.0.5");
+
+            Console.Write(Environment.ExpandEnvironmentVariables(
+                "=========================================================\n" +
+                "|\t\t\t\t\t\t\t|\n" +
+                "|\t%PROGRAMNAME%\t\t\t\t\t|\n" +
+                "|\t\t\t\t\t\t\t|\n" +
+                "|\tAuthor: SoftwareSpot (C) 2014\t\t\t|\n" +
+                "|\tBuild: 0.0.0.6\t\t\t\t\t|\n" +
+                "|\t\t\t\t\t\t\t|\n" +
+                "|\tUsage:\t\t\t\t\t\t|\n" +
+                "|\t%PROGRAMNAME%.exe \"HTMLFile/Folder\" < -allfiles >\t|\n" +
+                "|\t\t\t\t\t\t\t|\n" +
+                "|\t<optional> -allfiles: Display all files.\t|\n" +
+                "|\t\t\t\t\t\t\t|\n" +
+                "=========================================================\n"));
+
+            /* Error codes:
+             * 0 - Valid HTML/CSS file.
+             * 1 - Invalid HTML/CSS file.
+             *
+             * -1 - No file/directory was passed or the directory was empty.
+             * -2 - File/directory invalid.
+             * -3 - Unable to connect to the W3C Validator.
+             * -4 - User cancelled the operation.
+             */
+
+            // Constants for the exit message.
+            const sbyte EXIT_USER_CLOSE = -4, EXIT_W3C_CONNECTION = -3, EXIT_INVALID_PATH = -2, EXIT_EMPTY_PATH = -1, EXIT_W3C_INVALID = 0, EXIT_W3C_VALID = 1;
+            const byte FILECHECK_THRESHOLD = 5;
+
+            bool isErrorsWarningOnly = true; // Display only files with errors or warnings.
+            string searchPath = string.Empty;
+            if (GetCommandLineArgs(commandlineArgs, ref searchPath, ref isErrorsWarningOnly)) // Set the filePath as the one sent to the application as a commandline parameter.
+            {
+                searchPath = commandlineArgs[0];
+            }
+            else
+            {
+                Console.WriteLine(Environment.ExpandEnvironmentVariables("Please pass a valid HTML/CSS file or directory to %PROGRAMNAME%."));
+                Exit(EXIT_EMPTY_PATH);
+            }
+
+            bool isDir = IsDir(searchPath);
+            if (!isDir && !IsFile(searchPath))
+            {
+                Console.WriteLine(Environment.ExpandEnvironmentVariables("Please pass a valid HTML/CSS file or directory to %PROGRAMNAME%."));
+                Exit(EXIT_INVALID_PATH);
+            }
+
+            string[] fileList = new string[0]; // Create a new directory array.
+            if (isDir)
+            {
+                fileList = Directory.GetFiles(searchPath, "*.*", SearchOption.AllDirectories);
+                if (fileList.Length == 0) // If no files were found then exit with the exit code of EXIT_EMPTY_PATH.
+                {
+                    Exit(EXIT_EMPTY_PATH);
+                }
+            }
+            else // Otherwise it's a file as a check was done before to check if the file path was either a directory or file.
+            {
+                string[] tempFileList = { searchPath }; // Workaround for when a single HTML/CSS file is passed to HTMLValid.
+                fileList = tempFileList; // Set the directory array to the temporary array.
+                tempFileList = null; // Set to null to destroy the temporary array.
+            }
+
+            if (fileList.Length > FILECHECK_THRESHOLD)
+            {
+                Console.WriteLine("The directory contains more than {0} files and could take anywhere between " +
+                    "{1} second{2} to complete.\n" +
+                    "\n" +
+                    "This is a FREE service and thus W3C recommends a one second waiting period between uploads.\n" +
+                    "\n" +
+                    "Would you like to continue processing? (Y or N)", FILECHECK_THRESHOLD, fileList.Length, (fileList.Length > 1 ? "s" : ""));
+
+                string userChoice = string.Empty;
+                byte failCount = 0;
+                while (userChoice != "y" && userChoice != "n") // Continue to loop until y or n is entered.
+                {
+                    if (failCount > 0) // If the fail count is greater than zero then display a warning as to what is to be entered.
+                    {
+                        Console.WriteLine(); // Empty line.
+                        Console.Write("Please enter either Y or N: ");
+                    }
+                    userChoice = Console.ReadKey(true).KeyChar.ToString().ToLower(); // Get the char and convert to string and lowercase.
+                    failCount++;
+                }
+                if (userChoice == "n")
+                    Exit(EXIT_USER_CLOSE);
+            }
+
+            Console.WriteLine(); // Empty line.
+            Console.WriteLine("Validating . . .");
+            Console.WriteLine(); // Empty line.
+
+            sbyte exitCode = EXIT_W3C_INVALID; // Set the exit code to the default EXIT_W3C_INVALID, which is zero in this case.
+            int fileCount = 0, validCount = 0; // Set the file and valid count to zero.
+            HTMLValid htmlValid = new HTMLValid(Environment.ExpandEnvironmentVariables("%PROGRAMNAME%")); // Create a HTMLValid object and set the UserAgent.
+
+            Stopwatch totalTimer = Stopwatch.StartNew(); // Create a timer object.
+            foreach (string filePath in fileList) // As the directory array isn't being written to then a foreach() is preferred.
+            {
+                HTMLValidResults htmlResults = htmlValid.ValidateFilePath(filePath, true); // Returns a HTMLValidResults object.
+                if (htmlResults.FileType == HTMLValidFileType.NonSupportedFile) // If the file isn't a HTML or CSS file then skip the file.
+                {
+                    continue;
+                }
+
+                if (!htmlResults.IsConnected) // An error occurred with connecting to W3C so break from the loop.
+                {
+                    Console.WriteLine("An error occurred connecting to W3C's validation service.\n" +
+                        "Please try again or contact your local administrator.");
+                    exitCode = EXIT_W3C_CONNECTION;
+                    break;
+                }
+
+                validCount += (htmlResults.Status == HTMLValidStatus.Valid ? 1 : 0);
+                fileCount++;
+                if (isErrorsWarningOnly && htmlResults.Errors == 0 && htmlResults.Warnings == 0) // If only display errors and warnings and no errors or warnings found then continue.
+                {
+                    continue;
+                }
+
+                string fileName = PathCompactPathEx(filePath, 15);
+                Console.WriteLine("The {0} file {1} was {2}.", (htmlResults.FileType == HTMLValidFileType.CSS ? "CSS" : "HTML"), fileName, (htmlResults.Status == HTMLValidStatus.Valid ? "Valid" : "Invalid"));
+
+                if (htmlResults.Errors > 0 || htmlResults.Warnings > 0) // Print out the errors and/or warnings.
+                {
+                    Console.WriteLine("! It appears there are additional issues that should be addressed: ");
+                    if (htmlResults.Errors > 0)
+                    {
+                        Console.WriteLine("Errors: {0}", htmlResults.Errors);
+                    }
+                    if (htmlResults.Warnings > 0)
+                    {
+                        Console.WriteLine("Warnings: {0}", htmlResults.Warnings);
+                    }
+                }
+                Console.WriteLine(); // Empty line.
+
+                if (fileList.Length >= 2)
+                {
+                    Thread.Sleep(1000); // Recommendation by W3C is to sleep for 1 second, though this will only happen if there are 2 or more files.
+                }
+            }
+
+            htmlValid = null; // Destroy the reference to HTMLValid().
+
+            int seconds = (int)totalTimer.ElapsedMilliseconds / 1000; // Get the total number of elapsed seconds and cast as an integer.
+            if (exitCode == EXIT_W3C_INVALID) // If equal to zero then no major error occurred during the loop.
+            {
+                exitCode = (validCount == fileList.Length ? EXIT_W3C_VALID : EXIT_W3C_INVALID); // If all files were valid then set the exit code to EXIT_VALID_HTML.
+                Console.WriteLine("Created: {0}\n" +
+                    "Files: {1}\n" +
+                    "Valid: {2}\n" +
+                    "Running: {3} second{4}.", DateTime.Now, fileCount, validCount, seconds, (seconds == 1 ? "" : "s"));
+            }
+            Exit(exitCode);
+        }
+
+        private static string PathCompactPathEx(string filePath)
+        {
+            return PathCompactPathEx(filePath, 25);
+        }
+
+        private static string PathCompactPathEx(string filePath, int length)
+        {
+            if (!string.IsNullOrEmpty(filePath) && (filePath.Length - Path.GetFileName(filePath).Length) > length)
+            {
+                filePath = Regex.Replace(filePath, @"^(.{" + length + @"}).+?([^\\]+)$", @"$1...\$2"); // Shorten the path but still retaining the file name length.
+            }
+            return filePath;
+        }
+    }
+}
